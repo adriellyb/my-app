077f6f11d076d21ffebf5b56a64f34df2bc60f7e
adriellyb <adrielly0195@gmail.com>
2021-03-14 20:08:02 -0300

    + footer & header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,11 +2,15 @@ import React, { useEffect } from 'react';
 
 /** imports components */
 import Navbar from '../../components/NavBar/Navbar'
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
 
 export default function Home() {
     return(
         <div>
             <Navbar />
+            <Header />
+            <Footer />
         </div>
     );
 }
