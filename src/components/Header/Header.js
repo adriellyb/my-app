d274ab537639d749184d25d3fb7877ae2b419ebd
adriellyb <adrielly0195@gmail.com>
2021-03-14 20:09:36 -0300

    # defining section width

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Header.css';
+
+/** import photos */
+
+function Header() {
+    return(
+        <div className="hero"></div>
+    );
+}
+export default Header;
